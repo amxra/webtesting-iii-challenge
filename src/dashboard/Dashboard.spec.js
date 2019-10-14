6c7b54d09c7a89264e716595cdc7b42b0322993a
amxra <amiradediran@gmail.com>
2019-10-14 14:54:13 +0100

    Set up tests for all 3 components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Dashboard from '../dashboard/Dashboard';
+import Dashboard from './Dashboard';
 
 
 afterEach(rtl.cleanup);

2a41329439b8e65de79b007f1b5993ed2c5d56e6
amxra <amiradediran@gmail.com>
2019-10-14 13:29:35 +0100

    Set up forEach, afterEach. Define CleanUp

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -3,3 +3,17 @@ import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Dashboard from '../dashboard/Dashboard';
 
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+
+beforeEach(() =>{
+    wrapper = rtl.render(<Dashboard/>)
+})
+
+
+describe('Dashboard component', () => {
+
+})
